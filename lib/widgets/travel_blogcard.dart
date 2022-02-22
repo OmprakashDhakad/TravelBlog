b82b13f53131c00055db4e14281fa81dc2da4122
FlutLab.io <>
2022-02-22 10:14:49 +0000

    Autosave

--- lib/widgets/travel_blogcard.dart
+++ lib/widgets/travel_blogcard.dart
@@ -16,7 +16,7 @@ class TravelBlogCard extends StatelessWidget {
         return Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.only(right: 20.0, bottom: 10.0),
+              padding: const EdgeInsets.only(top: 10, right: 20.0, bottom: 10.0),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
                 child: Image.asset(
