69fbaad59f0b008285a1da207d3a62d469f6cbf0
FlutLab.io <>
2022-02-22 10:26:34 +0000

    Autosave

---
+++ lib/widgets/most_popular.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:app/model/travel.dart';
+
+class MostPopular extends StatelessWidget {
+  final _list = Travel.generateMostPopular();
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
