17a259056a2f70482a7f48720222a2df92fc58b9
FlutLab.io <>
2022-02-23 08:41:49 +0000

    Autosave

--- lib/details.dart
+++ lib/details.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("DetailPage"));
+  }
+}
