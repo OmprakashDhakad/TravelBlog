0991cdb830e362936ea2980ba0fa07f51f81550b
FlutLab.io <>
2022-02-23 09:17:42 +0000

    Autosave

--- lib/details.dart
+++ lib/details.dart
@@ -1,11 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'model/travel.dart';
 
 class DetailPage extends StatelessWidget {
   final Travel travel;
-  DetailPage({@required this.travel});
+  final double expandedHeight = 300;
+  DetailPage({required this.travel});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(child: Text("DetailPage")));
+    return Scaffold(
+        body: Stack(children: [
+      CustomScrollView(
+        slivers: [
+          _buildSilverHead()
+        ],
+      )
+    ]));
+  }
+
+  Widget _buildSilverHead() {
+    return SliverPersistentHeader(delegate: DetaileSliverDelegate(travel: travel, expandedHeight: expandedHeight));
+  }
+}
+
+class DetaileSliverDelegate extends SliverPersistentHeaderDelegate {
+  final Travel travel;
+  final double expandedHeight;
+  DetaileSliverDelegate({required this.travel, required this.expandedHeight});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      children: [
+        Opacity(opacity: 0.8, child: Image.asset(travel.url, width: MediaQuery.of(context).size.width, height: expandedHeight, fit: BoxFit.cover))
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => 0;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
